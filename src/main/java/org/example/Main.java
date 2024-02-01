4797813c4151dfd238c8d4f17bc33dfdd1d4371d
wzNaji <141943564+wzNaji@users.noreply.github.com>
2024-02-01 19:27:10 +0100

    New change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("isa demo");
+        System.out.println("this is a new change");
     }
 }
