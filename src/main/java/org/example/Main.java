e5559381cf392742bfc4331cb22840296894a9f1
wzNaji <141943564+wzNaji@users.noreply.github.com>
2024-02-01 20:08:57 +0100

    After new key

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("isa demo");
         System.out.println("this is a new change");
+
+        System.out.println("After new key xd ");
+
     }
 }
